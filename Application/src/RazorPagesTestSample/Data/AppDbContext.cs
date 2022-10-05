c85a94011b3d1caa7345aebd1d943f87b7c012dd
bcally <114932416+bcally@users.noreply.github.com>
2022-10-05 13:03:14 +0000

    message de commit

--- Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+/* ajout d'un super commentaire */
 
 namespace RazorPagesTestSample.Data
 {
